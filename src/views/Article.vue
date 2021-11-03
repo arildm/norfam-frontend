3710cf7eb3a91a30a7bfbb02a61f7c2542bbbbf8
Arild Matsson <arild.matsson@gu.se>
2021-11-03 06:58:19 +0100

    Facsimiles

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -11,6 +11,19 @@
         <article v-html="article.body" />
       </PaneContent>
     </Pane>
+    <div v-if="pages.length">
+      <h3>Faksimiler</h3>
+      <div class="facsimiles">
+        <a
+          v-for="fn in pages"
+          :key="fn"
+          :href="`http://runeberg.org/img/${fn}.3.png`"
+          class="facsimiles-item"
+        >
+          <img :src="`http://runeberg.org/img/${fn}.8.png`" />
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -33,6 +46,13 @@ export default {
   data: () => ({
     article: null,
   }),
+  computed: {
+    pages() {
+      return (
+        window[`scanned_${this.edition}`][this.article.title] || []
+      ).map((fn) => String(fn.match(/nf..\/\d+/)));
+    },
+  },
   async created() {
     this.article = await getArticle(this.edition, this.id);
   },
@@ -42,5 +62,23 @@ export default {
 <style scoped>
 h1 {
   margin-top: 0;
+}
+
+.facsimiles {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-right: -1rem;
+  margin-bottom: -1rem;
+}
+.facsimiles-item {
+  display: block;
+  flex: 1;
+  min-width: 10rem;
+  margin-right: 1rem;
+  margin-bottom: 1rem;
+}
+.facsimiles-item img {
+  width: 100%;
 }
 </style>
