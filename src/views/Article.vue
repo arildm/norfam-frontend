438328d68ac1bfbed01ee682d5547a3f4795cf29
Arild Matsson <arild.matsson@gu.se>
2021-12-03 16:20:47 +0100

    Cite-as for edition 1

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -12,6 +12,9 @@
       </PaneContent>
       <div class="article-pane-side">
         <PaneContent>
+          <div v-if="edition == 1" class="cite">
+            <p v-for="item in citeAs" :key="item">{{ item }}</p>
+          </div>
           <a v-if="linkRuneberg" :href="linkRuneberg" target="_blank">
             <ButtonLink icon="link" label="Läs på runeberg.org" />
           </a>
@@ -83,6 +86,38 @@ export default {
     },
     editionName() {
       return [null, "Utgåva 1", "Utgåva 2"][this.edition];
+    },
+    volumes() {
+      return {
+        nfaa: "1. A–Barograf (1876)",
+        nfab: "2. Barometer–Capitularis (1878)",
+        nfac: "3. Capitulum–Duplikant (1880)",
+        nfad: "4. Duplikator–Folkvandringen (1881)",
+        nfae: "5. Folkvisor–Grimnesmål (1882)",
+        nfaf: "6. Grimsby–Hufvudskatt (1883)",
+        nfag: "7. Hufvudskål–Kaffraria (1884)",
+        nfah: "8. Kaffrer–Kristdala (1884)",
+        nfai: "9. Kristendomen–Lloyd (1885)",
+        nfaj: "10. Lloyd–Militärkoloni (1886)",
+        nfak: "11. Militärkonventioner–Nådaval (1887)",
+        nfal: "12. Nådemedlem–Pontifikat (1888)",
+        nfam: "13. Pontin–Ruete (1889)",
+        nfan: "14. Ruff–Sockenstämma (1890)",
+        nfao: "15. Socker–Tengström (1891)",
+        nfap: "16. Teniers–Üxkull (1892)",
+        nfaq: "17. V–Väring (1893)",
+        nfar: "18. Värja–Öynhausen (1894)",
+        nfas: "19. supplement: A–Böttiger (1896)",
+        nfat: "20. supplement: C–Öxnevalla (1899)",
+      };
+    },
+    citeAs() {
+      return Object.keys(this.pages).map((volumeCode) => {
+        const pageRanges = this.pages[volumeCode].p
+          .map((range) => range.join("–"))
+          .join(", ");
+        return `Nordisk Familjebok, utgåva ${this.edition}, volym ${this.volumes[volumeCode]} ss. ${pageRanges}`;
+      });
     },
   },
   methods: {
